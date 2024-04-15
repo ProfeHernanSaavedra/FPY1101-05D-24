a90fea8fd5f6313216d7ef7ed3e3dc9b1bc88431
USRVI-LC2 <USRVI-LC2@duoc.acad>
2024-04-15 18:13:36 -0400

    primer codigo

---
+++ hola.py
@@ -0,0 +1,15 @@
+print("Hola mundo") # escribir
+print("Mi nombre es Profe Hernán")
+# comentario de linea
+print("********")
+print("Ingrese su nombre: ")
+nombre = input() # leer input()--> string o caracteres
+print("Hola, ",nombre," bienvenido")
+print("Cual es su edad:")
+edad = input() # numero ---> int(input()) INTEGER , float(input()) --> DECIMALES
+print("Su edad es :",edad)
+
+num1 = int(input("Ingrese un numero 1: "))
+num2 = int(input("Ingrese un numero 2: "))
+suma = num1 + num2
+print("La suma es: ", suma)
---
+++ promedio.py
@@ -0,0 +1,8 @@
+
+num1 = int(input("Ingrese un numero 1: "))
+num2 = int(input("Ingrese un numero 2: "))
+num3 = int(input("Ingrese un numero 3: "))
+suma = num1 + num2 + num3
+promedio = suma/3
+
+print("El promedio de ",num1,",",num2,",",num3," es: ",promedio)
