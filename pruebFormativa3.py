981ef0580a7c09ae6ec722943a5c3d94b0cf7378
Hernan Saavedra <hernan.saavedra@gmail.com>
2024-06-13 18:20:34 -0400

    ejercicio formativo 3

---
+++ pruebFormativa3.py
@@ -0,0 +1,24 @@
+import pruebaFormativa3_funciones as fn
+
+trabajadores = []
+
+while True:
+    print("Bienvenidos al sistema de sueldos")
+    print("1. Registrar trabajador")
+    print("2. Listar todos los trabajadores")
+    print("3. Imprimir planilla de sueldos")
+    print("4. Salir")
+
+    opcion = int(input("Seleccione su opción: "))
+
+    if opcion == 1:
+        fn.registrar_trabajador(trabajadores)
+    elif opcion == 2:
+        fn.listar_trabajadores(trabajadores)
+    elif opcion == 3:
+        fn.imprimir_planilla(trabajadores)
+        print("Planilla generada con exito")
+    elif opcion == 4:
+        break
+    else:
+        print("Opción no válida, seleccione nuevamente")
---
+++ pruebaFormativa3_funciones.py
@@ -0,0 +1,60 @@
+
+#definir la coleccion de los cargos
+CARGOS = ['ceo','desarrollador','analista de datos']
+
+#función registrar trabajador
+def registrar_trabajador(trabajadores):
+    nombre = input("Ingrese el nombre y apellido del trabajador: ")
+    cargo = input("Ingrese el cargo (CEO/Desarrollador/Analista de Datos): ").lower()
+    while cargo not in CARGOS:
+        print("Cargo no existe, vuelva a ingresar un cargo válido")
+        cargo = input("Ingrese el cargo (CEO/Desarrollador/Analista de Datos): ").lower()
+    sueldoBruto = int(input("Ingrese sueldo bruto del trabajador: "))
+
+    #calculo de valores
+    descSalud = sueldoBruto * 0.07
+    descAFP = sueldoBruto * 0.12
+    liquidoPagar = sueldoBruto-descSalud-descAFP
+
+    #almacenar estos datos en diccionario
+    trabajadores.append({
+        'Nombre' : nombre,
+        'Cargo' : cargo,
+        'SueldoBruto' : sueldoBruto,
+        'DescSalud' : descSalud,
+        'DescAFP' : descAFP,
+        'LiquidoPagar' : liquidoPagar 
+    })
+
+
+# función para listar todos los trabajadores.
+def listar_trabajadores(trabajadores):
+    print("Lista de trabajadores")
+    for trabajador in trabajadores:
+        print(trabajador)
+        #print(trabajador['Nombre'])
+
+def imprimir_planilla(trabajadores):
+    cargoSeleccionado = input("Ingrese el cargo para imprimir la planilla( o presiona ENTER para imprimir todo): ").lower()
+    if cargoSeleccionado == "":
+       trabajadores_a_imprimir = trabajadores
+       nombreArchivo = 'planilla_todos.txt'
+    elif cargoSeleccionado in CARGOS:
+        trabajadores_a_imprimir = []
+        for trabajador in trabajadores:
+           if trabajador['Cargo'] == cargoSeleccionado:
+               trabajadores_a_imprimir.append(trabajador)
+        nombreArchivo = f'planilla_{cargoSeleccionado}.txt'
+    else:
+        print("Cargo inválido")
+        return
+    
+    with open(nombreArchivo,'w') as archivo:
+        for trabajador in trabajadores_a_imprimir:
+            archivo.write(f"Nombre y Apellido : {trabajador['Nombre']}\n")
+            archivo.write(f"Cargo : {trabajador['Cargo']}\n")
+            archivo.write(f"Sueldo Bruto : ${trabajador['SueldoBruto']}\n")
+            archivo.write(f"Desc. Saludo : ${trabajador['DescSalud']}\n")
+            archivo.write(f"Desc. AFP : ${trabajador['DescAFP']}\n")
+            archivo.write(f"Liquido a Pagar : ${trabajador['LiquidoPagar']}\n\n")
+
